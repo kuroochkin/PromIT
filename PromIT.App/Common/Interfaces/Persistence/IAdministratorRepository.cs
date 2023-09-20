875a858f7dde4191eff80705c797364512ccfb65
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-09-20 19:03:41 +0300

    Создание интерфесов в Persistence

---
+++ PromIT.App/Common/Interfaces/Persistence/IAdministratorRepository.cs
@@ -0,0 +1,7 @@
+﻿using PromIT.Domain.Administrator;
+
+namespace PromIT.App.Common.Interfaces.Persistence;
+
+public interface IAdministratorRepository : IGenericRepository<AdministratorEntity>
+{
+}
