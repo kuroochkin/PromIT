029c6eeb2a8437cb8fe4f4c31b04458e20d22c49
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-09-20 19:11:30 +0300

    Добавление контекста БД

---
+++ PromIT.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PromIT.Domain.Administrator;
+using PromIT.Domain.Review;
+using PromIT.Domain.Reviewer;
+using PromIT.Domain.User;
+
+namespace PromIT.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+	public DbSet<UserEntity> Users { get; set; }
+	public DbSet<ReviewerEntity> Reviewers { get; set; }
+	public DbSet<AdministratorEntity> Administrators { get; set; }
+	public DbSet<ReviewEntity> Reviews { get; set; }
+
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+	   : base(options) { }
+}
